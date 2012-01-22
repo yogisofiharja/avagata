6996ac7276dc64f2daf76f89558bfc097405782c
yogi sofi harja <hardja.ogie@yahoo.com>
2012-01-23 00:16:52 +0700

    atur template

--- application/views/polos/konten.php
+++ application/views/polos/konten.php
@@ -1,20 +1,6 @@
   <div class="body">
-    <div class="bgg1">
-      <div class="left1">
-        <h2>Events &amp; Updates</h2>
-        <p><strong>Posted on 20 May 09 </strong><br />
-          Listuem Names ligula a blandit ornare ligula a quis 
-          bibendum elit anten ecm etus blandit</p>
-        <p><strong>Posted on 24 May 09 </strong><br />
-          Nulla elit porttitor tellus quis bibendum elit ante nec 
-          metus.</p>
-        <p><a href="#"><img src="images/more_new.gif" alt="picture" width="94" height="16" border="0" /></a></p>
-        <p>&nbsp;</p>
-        <h2>Testimonials</h2>
-        <p><em>“It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. Content here, content here', making it look like readable English.”</em></p>
-        <p><strong>-John Smith, webdesigner</strong></p>
-      </div>
-      <div class="right1">
+    <div class="bgg">     
+      <div class="right">  
         <h3>Welcome to our company</h3>
         <img src="images/img_1.jpg" alt="picture" width="145" height="145" class="floated" />
         <p><strong>It has survived not only five centuries, but also the leap into <br />
