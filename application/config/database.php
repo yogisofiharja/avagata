f8342d521fe2f2c73c36c77f0cef4fafb429c41c
yogi sofi harja <hardja.ogie@yahoo.com>
2012-01-22 20:28:41 +0700

    ganti user dan pass database

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
+$db['default']['username'] = 'avagata';
+$db['default']['password'] = 'pass4avagata';
 $db['default']['database'] = 'avagata';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
