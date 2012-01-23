49a177350f0c277313e9e4d8f473100a203daad3
yogi sofi harja <hardja.ogie@yahoo.com>
2012-01-24 03:41:36 +0700

    tes

--- application/views/downloads.php
+++
@@ -1,5 +0,0 @@
-<?php $this->load->view('base/header1');?>
-<?php $this->load->view('base/header2');?>
-<?php $this->load->view('biasa/side');?>
-<?php $this->load->view('biasa/konten');?>
-<?php $this->load->view('base/footer');?>
--- application/views/services.php
+++
@@ -1,5 +0,0 @@
-<?php $this->load->view('biasa/header1');?>
-<?php $this->load->view('biasa/header2');?>
-<?php $this->load->view('biasa/konten');?>
-<?php $this->load->view('biasa/side');?>
-<?php $this->load->view('biasa/footer');?>
